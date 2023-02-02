39ca66402cc47ac14c0771ce66b89e52eb22cffe
Negodn1k <havkaset@gmail.com>
2023-02-02 14:46:53 +0300

    v 1.0

---
+++ src/Main.java
@@ -0,0 +1,56 @@
+import Sort.BubleSort.Bubble;
+import Sort.EnterSort.Enter;
+import Sort.FastSort.FastSort;
+import Sort.SelectSort.Select;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> array = new ArrayList<>();
+        Bubble bubble = new Bubble();
+        FastSort fastSort = new FastSort();
+        Enter enter = new Enter();
+        Select select = new Select();
+
+        array.add(5);
+        array.add(4);
+        array.add(3);
+        array.add(2);
+        array.add(1);
+        System.out.println(array);
+
+        bubble.go(array);
+        System.out.println(array);
+
+        array.set(0,5);
+        array.set(1,4);
+        array.set(2,3);
+        array.set(3,2);
+        array.set(4,1);
+        System.out.println(array);
+
+        fastSort.go(array,0, array.size() - 1);
+        System.out.println(array);
+
+        array.set(0,5);
+        array.set(1,4);
+        array.set(2,3);
+        array.set(3,2);
+        array.set(4,1);
+        System.out.println(array);
+
+        enter.enterSort(array,0,array.size() - 1);
+        System.out.println(array);
+
+        array.set(0,5);
+        array.set(1,4);
+        array.set(2,3);
+        array.set(3,2);
+        array.set(4,1);
+        System.out.println(array);
+
+        select.go(array);
+        System.out.println(array);
+    }
+}
